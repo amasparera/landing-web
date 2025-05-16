1eacd63d8f266cf2daec8cd3b7d1994e73dd359d
amasparera <amazparera@gmail.com>
2025-05-16 15:41:05 +0700

    add new course

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrondController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+// Route::get('/frond',[FrondController::class,'index'])->name('frond.index');
